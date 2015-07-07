d033c7bae2cc72b53e0ee0cfcad85aad3cc36b6b
Abdullah Al Ahad <ahadcse@gmail.com>
2015-07-07 17:27:12 +0200

    Added response status annotation

--- src/main/java/com/tradedoubler/application/controller/GreetingController.java
+++ src/main/java/com/tradedoubler/application/controller/GreetingController.java
@@ -4,9 +4,8 @@ import com.tradedoubler.application.model.Greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import static com.tradedoubler.application.constant.ControllerConstant.GREETING;
 
@@ -16,13 +15,16 @@ import static com.tradedoubler.application.constant.ControllerConstant.GREETING;
  */
 
 /**
+ * TODO Change default port
+ * TODO Add properties file
  * TODO need to add unit test
- * TODO Add slf4j for logging
+ * TODO Add slf4j for logging  (See openplatform uts)
  * TODO Customize data format (See openplatform uts)
  * TODO Response code
  * TODO Error handling
  * TODO tokenize
  * TODO add mySQL DB
+ * TODO Add Swaggar
  */
 
 @RestController
@@ -31,7 +33,9 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value = GREETING)
+    @RequestMapping(value = GREETING, method = RequestMethod.GET)
+    // Ensures that HTTP requests to /greeting are mapped to the greeting() method.
+    @ResponseStatus(value = HttpStatus.OK)
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "Developer") String name,
                              @RequestParam(value = "greetingText", defaultValue = "Welcome") String greetingText) {
         return new Greeting(counter.incrementAndGet(),
