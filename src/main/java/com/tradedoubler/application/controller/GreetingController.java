921294a47e524f5e1144ab14b1cdd8705e51ed09
Abdullah Al Ahad <abdullah.alahad@tradedoubler.com>
2015-09-22 12:08:57 +0200

    changed a variable name

--- src/main/java/com/tradedoubler/application/controller/GreetingController.java
+++ src/main/java/com/tradedoubler/application/controller/GreetingController.java
@@ -1,12 +1,14 @@
 package com.tradedoubler.application.controller;
 
 import com.tradedoubler.application.model.Greeting;
+import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 import static com.tradedoubler.application.constant.ControllerConstant.GREETING;
+import static org.slf4j.LoggerFactory.getLogger;
 
 
 /**
@@ -15,26 +17,28 @@ import static com.tradedoubler.application.constant.ControllerConstant.GREETING;
 
 /**
  * TODO need to add unit test
+ * TODO add mySQL DB. Use spring in memory database
+ * TODO Add more rest interfaces
  * TODO Customize data format (See openplatform uts)
  * TODO Response code
  * TODO Use websocket
  * TODO Error handling
  * TODO tokenize. Configure token in properties file
- * TODO add mySQL DB. Use spring in memeory database
  */
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final Logger LOGGER = getLogger(GreetingController.class);
+    private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    // Ensures that HTTP requests to /greeting are mapped to the greeting() method.
     @RequestMapping(value = GREETING, method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "Developer") String name,
                              @RequestParam(value = "greetingText", defaultValue = "Welcome") String greetingText) {
+        LOGGER.info("greeting controller executed...");
         return new Greeting(counter.incrementAndGet(),
-                String.format(template, name), greetingText);
+                String.format(TEMPLATE, name), greetingText);
     }
 }
